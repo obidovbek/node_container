a3ea43919cb6f020ac31de730dcf1b3aafbd5fb1
obidovbek <obidov.bekzod94@gmail.com>
2023-11-29 06:53:01 +0500

    route commit

---
+++ src/common/route.interface.ts
@@ -0,0 +1,11 @@
+import { NextFunction, Router, Request, Response} from "express";
+import { IMiddleware } from "./middleware.interface";
+
+export interface IControllerRoute{
+    path:string;
+    func: (req:Request, res:Response, next:NextFunction)=> void,
+    method: keyof Pick<Router, 'get' | 'post' | 'delete' | 'patch' | 'put'>,
+    middlewares: IMiddleware[];
+}
+
+export type ExpressReturnType = Response<any, Record<string,any>>;
